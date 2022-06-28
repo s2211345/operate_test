ce6bd86a2b432ef557142895a2c7169222d2fd70
s2211345 <610268414@qq.com>
2022-06-28 23:12:52 +0800

    新建ttt分支提交

--- src/main/java/com/test/controller/Test.java
+++ src/main/java/com/test/controller/Test.java
@@ -1,12 +1,12 @@
 package com.test.controller;
 
+import java.util.List;
+
 /**
  *  Test  1123
  */
 public class Test{
     private String name;
     private Integer age;
-    private String sex;
-    private String ttt2;
 	public List list;
 	public String ss;	private String str;}
